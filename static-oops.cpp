4b5a70900111459b27d03a023e44bc747860b587
Mintu <pmk80028@gmail.com>
2026-05-23 14:11:42 +0530

    Implemented a simple C++ program to demonstrate static variable storage

---
+++ static-oops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void fun(){
+    static int x = 0;
+    cout <<"x : " << x << endl;
+    x++;
+}
+
+int main (){
+    fun();
+    fun();
+    fun();
+    return 0;
+}
